866712dc66d4d51193df8cce07c31672a190b2ed
Luqueli <54374762+Luqueli@users.noreply.github.com>
2021-07-25 22:51:02 +0200

    Add files via upload

---
+++ prueba_tecnicabdd.sql
@@ -0,0 +1,29 @@
+create database prueba_tecnicabdd;
+use prueba_tecnicabdd;
+
+# Tabla Producto
+create table Producto(
+	codigo_p int NOT NULL,
+    nombre_p varchar(50) NOT NULL,
+    precio_p float NOT NULL,
+    categoria_p varchar(50) NOT NULL,
+    constraint pk_cp primary key(codigo_p)
+);
+
+#Tabla Vendedor
+create table Vendedor(
+	id_v int NOT NULL AUTO_INCREMENT,
+    nombre_v varchar(50) NOT NULL,
+    sueldo_v float NOT NULL,
+    constraint pk_idv primary key(id_v)
+);
+
+# Tabla Venta
+create table Venta(
+	codigo_v int NOT NULL AUTO_INCREMENT,
+    id_vendedor_v int NOT NULL,
+    codigo_producto_v int NOT NULL,
+    constraint pk_cv primary key(codigo_v),
+    constraint fk_ivv foreign key(id_vendedor_v) references Vendedor (id_v),
+    constraint fk_cpp foreign key(codigo_producto_v) references Producto (codigo_p)
+)
